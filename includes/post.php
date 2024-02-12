65eb7e75abac4bd1423e99664d5162637035f903
Antoine Kaufmann <antoinek@cs.washington.edu>
2024-02-12 13:52:07 +0000

    add archive

--- includes/post.php
+++ includes/post.php
@@ -2,8 +2,13 @@
 
 function jkpg_init_post() {
   register_post_type('jkpg', array(
+      'public' => true,
       'publicly_queryable' => true,
       'exclude_from_search' => false,
+      'has_archive' => 'gallery',
+      'labels' => array(
+        'archives' => 'Albums'
+      )
   ));
 }
 add_action( 'init', 'jkpg_init_post' );
@@ -18,3 +23,14 @@ function jkpg_post_template( $single_template ) {
 	return $single_template;
 }
 add_filter( 'single_template', 'jkpg_post_template' );
+
+function jkpg_archive_template( $archive_template ) {
+	global $post;
+
+	if ( is_post_type_archive('jkpg') ) {
+		$archive_template = dirname( __FILE__ ) . '/../templates/archive-jkpg.php';
+	}
+
+	return $archive_template;
+}
+add_filter( 'archive_template', 'jkpg_archive_template' );
---
+++ templates/archive-jkpg.php
@@ -0,0 +1,55 @@
+<?php
+get_header();
+
+$pics = [];
+$imgbase = '/wp-content/uploads/jkpg/';
+?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+    <div class="jkpg-grid grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 280 }'>
+    <?php
+    while ( have_posts() ) : the_post();
+      $aid = get_post_meta(get_the_ID(), 'jkpg_album', true);
+      $alb = jkpg_db_album_get($aid);
+      $pids = jkpg_db_p2a_pic_ids($aid);
+      $pic = jkpg_db_pic_get($pids[0]);
+      $pics[] = $pic;
+      $sizes = explode(",", $pic->sizes);
+      natsort($sizes);
+      $sz = $sizes[array_key_first($sizes)];
+      $fn = $imgbase . $pic->adobe_id . '-' . $sz . '.jpg';
+      $szp = explode("x", $sz);
+      $w = $szp[0];
+      $h = $szp[1];
+      ?>
+      <div class="grid-item">
+        <a href="<?php echo get_permalink(get_the_ID()); ?>">
+          <span class="title"><?php echo esc_html($alb->title); ?></span>
+          <img src="<?php echo $fn; ?>"
+            width="<?php echo $w; ?>"
+            height="<?php echo $h; ?>"
+            title="<?php echo esc_attr($alb->title); ?>"
+            alt="<?php echo esc_attr($alb->title); ?>">
+        </a>
+      </div>
+      <?php
+		endwhile; // end of the loop.
+    ?>
+    </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+  <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+  <script>
+    // init Masonry
+    var $grid = $('.grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: 280,
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
+  </script>
+<?php get_footer(); ?>
